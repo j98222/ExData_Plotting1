c20b3c82a8fc6330a4f39593346750de55259a3c
j98222 <j9822@hotmail.com>
2017-12-30 17:20:06 -0700

    correct polt1 date issue and upload polt2

--- plot2.R
+++ plot2.R
@@ -5,13 +5,15 @@ readData = function (filename){
     result
 }
 
-
 title = read.table("household_power_consumption.txt",header= FALSE,nrows=1,sep=";")
 v = as.matrix(title)[1,]
 temp = readData("household_power_consumption.txt")
 d = read.table(text=temp,sep=";",na="?")
+
 names(d)=v
 d$Global_active_power = as.numeric(d$Global_active_power)
-png(file = "plot1.png", width = 480, height = 480, units = "px")
-hist(d$Global_active_power,col="red",main="Global Active Power",xlab = "Global Active Power(kilowatts)")
+d$Date <- as.Date(d$Date, format = "%d/%m/%Y")
+d$week <- as.POSIXct(strptime(paste(d$Date, d$Time, sep = " "),format = "%Y-%m-%d %H:%M:%S"))
+png(file = "plot2.png", width = 480, height = 480, units = "px")
+with(d,plot(week,Global_active_power,type = "l",xlab = "",ylab = "Global Active Power (kilowatts)"))
 dev.off()
